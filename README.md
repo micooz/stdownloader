7785f96181b54d51bffccd994a7c69e23bb8e7c3
Micooz <micooz@hotmail.com>
2015-01-09 14:27:39 +0800

    添加CMakeLists以进行跨平台编译。

--- README.md
+++ README.md
@@ -1,1 +1,200 @@
-#StDownloader
+# StDownloader
+C++实现的跨平台 songtaste.com 音乐下载工具
+
+# 开发环境
+
+QtCreator with MingGW 4.8 （32bit）
+
+# 使用的库
+
+* boost 1.56 （未包含在项目内）
+
+* avhttp 2.9.9
+
+* jsoncpp 0.7.0
+
+* ffmpeg （提供二进制程序）
+
+# 编译
+
+下面简单介绍这套工具在`Windows`以及`Linux`下的编译方法。
+
+编译前必须具备下列条件：
+
+* boost_1_56_0 及以上
+
+* cmake 2.8 及以上
+
+* 编译器 MinGW（Windows） 4.8 及以上；g++ 4.8（Linux）及以上
+
+## 编译boost
+
+boost库官方网站提供了最新版本的boost编译方法：
+
+### Windows
+
+http://www.boost.org/doc/libs/1_57_0/more/getting_started/windows.html#prepare-to-use-a-boost-library-binary
+
+### Unix（Linux，MacOS）
+
+http://www.boost.org/doc/libs/1_57_0/more/getting_started/unix-variants.html#easy-build-and-install
+
+## 编译jsoncpp
+
+### Windows
+
+```
+$ cd ./libs/jsoncpp-0.7.0/
+$ cmake . -G "MinGW Makefiles"
+$ mingw32-make
+```
+
+完成后在`lib`下可以找到`libjsoncpp.a`
+
+### Linux
+
+```
+
+```
+
+## 编译StDownloader
+
+虽然使用Qt（qmake）进行开发，但为了简便，使用`cmake`进行快速构建。
+
+### Windows
+
+下面以MinGW为例，假定boost根目录在`D:/libs/boost_1_56_0`（见CMakeLists.txt）
+
+在项目根目录执行：
+
+```
+$ cmake . -G "MinGW Makefiles"
+$ mingw32-make
+```
+
+如果找不到boost相关头文件，请在顶层的`CMakeLists`中修改`BOOST_ROOT`设置。
+
+### Linux
+
+```
+
+```
+
+# 工具简介
+
+主要提供一主入口（songtaste）、列表（list）、下载（down）、解析（parse）工具。
+
+## 主入口（songtaste）
+
+入口程序是调用其他附属工具的途径之一，执行命令时仅需要给入口传递子程序名和相应参数即可。
+
+## 列表（list）
+
+目前列表功能支持“大家推荐”、“音乐分类”、“一周排行”的相关列表和音乐列表，支持分页。
+
+为方便之后开发的GUI程序的解析，所有结果均以json格式输出。所有列表均支持缓存。
+
+### 使用示例
+
+**查看帮助**
+
+```
+$ songtaste list
+or
+$ songtaste list --help
+```
+
+**大家推荐**
+
+```
+$ songtaste list --recommend
+#分页
+$ songtaste list --recommend --page 3
+```
+
+**分类列表**
+
+```
+$ songtaste list --category
+```
+
+**分类中的音乐列表**
+
+```
+$ songtaste list --catsong --catid 2
+$ songtaste list --catsong --catid 2 --page 2
+```
+
+**周列表**
+
+```
+$ songtaste list --week
+```
+
+**本周最佳歌曲**
+
+```
+$ songtaste list --weekchart
+```
+
+需要缓存功能可以在 **config.json** 中配置缓存路径，然后给命令添加 **--cache** 选项。
+
+## 下载（down）
+
+下载程序可以实时显示下载进度和下载速度，并给出文件大小等必要信息。
+
+### 使用示例
+
+**查看帮助**
+
+```
+$ songtaste down
+```
+
+**下载指定id的音乐**
+
+```
+$ songtaste down --id 3468640
+```
+
+**获取指定id的音乐信息**
+
+```
+$ songtaste down --id 3468640 --info
+```
+
+**下载到指定目录**
+
+```
+$ songtaste down --id 3468640 --dir ./
+```
+
+**下载到指定目录并重命名** （原始名可能是非法的文件名）
+
+```
+$ songtaste down --id 3468640 --dir ./music --name my_music
+```
+
+## 解析（parse）
+
+通过ffmpeg解析音乐可以得到原始的歌名作者等信息。
+
+### 使用示例
+
+**查看帮助**
+
+```
+$ songtaste parse
+```
+
+**解析指定音乐** （将间接调用ffmpeg）
+
+```
+$ songtaste parse test.mp3
+```
+
+# 免责声明
+
+本工具所取得的资源均来自互联网，仅供学习交流。
+
+如在使用过程中侵犯了原音乐的版权，请予以删除，未经原版权作者许可，任何人不得擅作它用！请在下载24小时内删除！为尊重作者版权，请购买原版作品。
--- parse/main.cpp
+++ parse/main.cpp
@@ -12,36 +12,38 @@ using namespace std;
 
 const string help = "Usage: parse audio-file";
 
-typedef map<string,string>      metaData;
+typedef map<string, string>      metaData;
 typedef shared_ptr<metaData>    MetaData;
 
-static MetaData parse(const char *buf) {
-    auto *config = songtaste::Configure::getInstance();
-    string pattern_1 = config->all()["parse"]["first"].asString();
-    string pattern_2 = config->all()["parse"]["second"].asString();
-    
-    if(config->all().empty()){
+static MetaData parse(const char *buf)
+{
+    songtaste::Configure &config = *(songtaste::Configure::getInstance());
+    string pattern_1 = config["parse"]["first"].asString();
+    string pattern_2 = config["parse"]["second"].asString();
+
+    if (pattern_1.empty() || pattern_2.empty()) {
         throw logic_error("config error");
     }
-    
+
+
     string org(buf);
     metaData *pdata = new metaData;
     boost::regex pattern(pattern_1);
     boost::smatch matches;
-    
+
     std::string::const_iterator s = org.begin();
     std::string::const_iterator e = org.end();
-    
+
     while (boost::regex_search(s, e, matches, pattern)) {
         (*pdata)[matches.str(1)] = matches.str(2);
         s = matches[2].second;
     }
-    
+
     pattern.assign(pattern_2);
-    
+
     s = org.begin();
     e = org.end();
-    
+
     while (boost::regex_search(s, e, matches, pattern)) {
         (*pdata)["duration"]  = matches.str(1);
         (*pdata)["start"   ]  = matches.str(2);
@@ -49,26 +51,27 @@ static MetaData parse(const char *buf) {
         (*pdata)["audio"   ]  = matches.str(4);
         s = matches[4].second;
     }
-    
+
     return MetaData(pdata);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     try {
         if (argc != 2) {
             cout << help;
             exit(0);
         }
-        
+
         string audio = boost::filesystem::system_complete(boost::filesystem::path(argv[1])).string();
         string command("ffmpeg -i ");
         command.append(audio);
         command.append(" 2>&1");
-        
+
         FILE *pf = nullptr;
         char buffer[1024 * 5];
         memset(buffer, 0, sizeof(buffer));
-        
+
 #if defined (WIN32) || defined (_WIN32)
         pf = _popen(command.c_str(), "r");
 #elif defined (linux)||defined (__linux__)
@@ -77,29 +80,29 @@ int main(int argc, char *argv[]) {
         if (!pf) {
             throw logic_error("cannot access ffmpeg");
         }
-        
+
         fread(buffer, sizeof(char), sizeof(buffer), pf);
-        
+
 #if defined (WIN32) || defined (_WIN32)
         _pclose(pf);
 #elif defined (linux)||defined (__linux__)
         pclose(pf);
-#endif 
+#endif
         pf = nullptr;
-        
+
         MetaData dataset = parse(buffer);
         Json::Value json;
-        for(auto it = dataset->cbegin();
-            it != dataset->cend(); ++it){
+        for (auto it = dataset->cbegin();
+             it != dataset->cend(); ++it) {
             json[(*it).first] = (*it).second;
         }
         Json::FastWriter writer;
-        
+
         cout << writer.write(json);
-        
+
     } catch (const std::exception &err) {
         cout << err.what() << endl;
     }
-    
+
     return 0;
 }
